c927fda8c9c06589b4879307e7a9fd30a81f8bd5
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-10-20 15:29:58 +0900

    feat: 백엔드 프로젝트 생성

---
+++ back/src/main/java/com/closeai/ecoprompt/EcoPromptApplication.java
@@ -0,0 +1,13 @@
+package com.closeai.ecoprompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoPromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoPromptApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/closeai/ecoprompt/EcoPromptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.closeai.ecoprompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoPromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
