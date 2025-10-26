3ccf82234623b4c3a0987563fd455f33402311c3
epnjh0807 <epnjh0807@gmail.com>
2025-10-27 00:39:28 +0900

    fix: App.tsx 레이아웃 구조 정리
    - 불필요한 Link import 제거
    - Topbar, Bottombar, Sidebar 컴포넌트 통합
    - 반응형 레이아웃 적용 (모바일/태블릿/데스크탑)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,27 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Header from '@/components/common/Topbar'
+import Bottombar from '@/components/common/Bottombar'
+import Sidebar from '@/components/common/Sidebar'
 
 function App() {
   return (
-    <div className="mx-auto max-w-3xl p-6">
-      <nav className="mb-6 flex gap-4">
-        <Link className="text-blue-600 hover:underline" to="/">Home</Link>
-        <Link className="text-blue-600 hover:underline" to="/about">About</Link>
-      </nav>
-      <Outlet />
+    <div className="flex h-screen">
+      {/* Sidebar는 모바일/태블릿에서 오버레이, 데스크탑에서 레이아웃에 포함 */}
+      <Sidebar />
+
+      {/* 메인 콘텐츠 영역 */}
+      <div className="flex flex-col flex-1 overflow-hidden">
+        {/* TopBar: 모바일 + 태블릿에만 표시 */}
+        <Header />
+
+        {/* 메인 콘텐츠 */}
+        <main className="flex-1 overflow-auto p-6 pt-[10vh] pb-[12vh] md:pt-[10vh] md:pb-6 lg:pt-6 lg:pb-6">
+          <Outlet />
+        </main>
+
+        {/* BottomBar: 모바일에만 표시 */}
+        <Bottombar />
+      </div>
     </div>
   )
 }
