e5c4f56bef04d62d5ef94746500b23c3e3ecae4d
이권민 <rnjsals0905@gmail.com>
2025-10-28 14:41:07 +0900

    feat: 프로파일 기반 db 연결 변경 기능 추가

--- back/src/main/java/com/closeai/ecoprompt/config/SshDataSourceConfig.java
+++ back/src/main/java/com/closeai/ecoprompt/config/SshDataSourceConfig.java
@@ -7,10 +7,12 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Slf4j
+@Profile("dev")
 @Configuration
 @RequiredArgsConstructor
 public class SshDataSourceConfig {
--- back/src/main/java/com/closeai/ecoprompt/config/SshTunnelingInitializer.java
+++ back/src/main/java/com/closeai/ecoprompt/config/SshTunnelingInitializer.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
@@ -16,6 +17,7 @@ import static java.lang.System.exit;
 
 @Slf4j
 @Component
+@Profile("dev")
 @ConfigurationProperties(prefix = "ssh")
 @Validated
 @Setter
