2537fa0f6ac4820e0dadae0ce4ed40e0052773b4
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-10-30 14:51:32 +0900

    feat: SecurityConfig에 CSRF 설정 추가

--- back/src/main/java/com/closeai/ecoprompt/config/SecurityConfig.java
+++ back/src/main/java/com/closeai/ecoprompt/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,8 +19,11 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(authorize -> authorize
 			.requestMatchers(SWAGGER_URLS).permitAll()
+			.requestMatchers("/api/v1/**").permitAll()
 			.anyRequest().authenticated());
 
 		return http.build();
