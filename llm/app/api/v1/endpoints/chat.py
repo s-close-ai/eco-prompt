7d4378df1b4c2a9906ffe29cc7e5c9fb70f78327
surin <tjdtnfls1128@gmail.com>
2025-11-06 16:49:02 +0900

    feat: 사용자 히스토리 불러오기
    - pymongo를 이용한 사용자 히스토리 불러오기
    - 비선호답변(rejected response) 백엔드 저장으로 변경
    - 시스템 프롬프트 수정을 통해 답변 받는 형식 확정

--- llm/app/api/v1/endpoints/chat.py
+++ llm/app/api/v1/endpoints/chat.py
@@ -1,37 +1,48 @@
+import json
 from fastapi import APIRouter, Depends
 from fastapi.responses import StreamingResponse
+from fastapi.exceptions import HTTPException
 
 from app.schemas.chat import ChatRequest, ChatResponse
 from app.services.chat import stream_response
 from app.models.llm_loader import get_llm, get_tokenizer
 from app.models.vectordb_loader import get_vector_store
-from app.models.mongodb_loader import find_chatting_id, get_chat_history
+from app.models.mongodb_loader import get_mongodb
+from app.services.use_mongodb import find_chatting_id, get_chat_history
 
 router = APIRouter()
 
 @router.post("")
-async def chat(request: ChatRequest, llm=Depends(get_llm), tokenizer=Depends(get_tokenizer), vector_store=Depends(get_vector_store)):
+async def chat(request: ChatRequest, llm=Depends(get_llm), tokenizer=Depends(get_tokenizer), vector_store=Depends(get_vector_store), mongo_client=Depends(get_mongodb)):
     """
     스트림 답변 제공
     """
     user_input = request.user_input
     personal_prompt = request.personal_prompt
-    message_uuid = request.message_uuid
+    message_uuid = str(request.message_uuid)
+    
+    try:
+        # 사용자 대화 히스토리 불러오기
+        chatting_id = await find_chatting_id(mongo_client, message_uuid)
+        print(f"[채팅 번호] {chatting_id}")
 
-    # 사용자 대화 히스토리 불러오기
-    chatting_id = find_chatting_id(str(message_uuid))
-    if chatting_id:
-        chat_history = get_chat_history(int(chatting_id))
-        # content와 senderType을 조합해서 histroy 생성하는 코드 필요함.
-        chat_history = "히스토리 요약 생성 예정"
-    else:
+        chat_history = await get_chat_history(mongo_client, int(chatting_id))
+        print(f"[채팅 기록] {chat_history}")
+
+    except Exception as e:
+        print(f"ERROR {e}")
+        chatting_id = None
+        print(f"[채팅 번호] {chatting_id}\n=> 채팅 기록이 없습니다.")
         chat_history = ""
 
+    # 답변 생성 체인
     chain = stream_response(vector_store=vector_store, llm=llm, tokenizer=tokenizer)
+    
     payload = {
         "personal_prompt": personal_prompt,
         "question": user_input,
         "history": chat_history,
+        "context": ""    # 벡터 DB 연결해봐야 함.
     }
 
     async def event_generator():
@@ -47,6 +58,7 @@ async def chat(request: ChatRequest, llm=Depends(get_llm), tokenizer=Depends(get
         sequence_id = -1
         try:
             async for chunk in chain.astream(payload):
+                # print(chunk)
                 if not chunk:
                     continue
                 
@@ -66,8 +78,15 @@ async def chat(request: ChatRequest, llm=Depends(get_llm), tokenizer=Depends(get
                 
                 if (state == "FOUND_CHOSEN") and (OPEN_C not in chunk) and (CLOSE_C in chunk):
                     state = "END_CHOSEN"
-                    yield f"data: {ChatResponse(sequence_id=sequence_id, token='DONE').model_dump_json()}\n\n"
-                    continue
+                    if chunk.strip() != CLOSE_C:
+                        chunk = chunk.replace(CLOSE_C, "")
+                        yield f"data: {ChatResponse(sequence_id=sequence_id, token=chunk).model_dump_json()}\n\n"
+                        yield f"data: {ChatResponse(sequence_id=sequence_id+1, token='DONE').model_dump_json()}\n\n"
+                        continue
+
+                    else:
+                        yield f"data: {ChatResponse(sequence_id=sequence_id, token='DONE').model_dump_json()}\n\n"
+                        continue
 
 
                 if (state == "END_CHOSEN") and (OPEN_R in chunk):
@@ -76,6 +95,7 @@ async def chat(request: ChatRequest, llm=Depends(get_llm), tokenizer=Depends(get
 
 
                 if (state == "FOUND_REJECTED") and (OPEN_R not in chunk) and (CLOSE_R not in chunk):
+                    # print(chunk)
                     rejected_response += chunk
                     continue
 
@@ -84,13 +104,14 @@ async def chat(request: ChatRequest, llm=Depends(get_llm), tokenizer=Depends(get
                     break
 
             print(f"[CHOSEN]\n{chosen_response}")
+            print(f"[REJECTED]\n{rejected_response}")
+
+            yield f"data: {ChatResponse(sequence_id=-1, token=rejected_response).model_dump_json()}\n\n"
         
         except Exception as e:
+            # print(chunk)
             # 오류 시 스트림 종료
             yield f"data: [ERROR] {type(e).__name__}: {e}\n\n"
 
-        print(f"[REJECTED]\n{rejected_response}")
-
-        # rejected response 저장하기
-
     return StreamingResponse(event_generator(), media_type="text/event-stream")
+
