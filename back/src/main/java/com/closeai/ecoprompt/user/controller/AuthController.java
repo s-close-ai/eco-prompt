94ee233a9bc0d2c5afba6a29346f6534ba754703
이권민 <rnjsals0905@gmail.com>
2025-11-07 17:57:08 +0900

    docs: swagger 문서 추가 (& UserController.java 삭제)

--- back/src/main/java/com/closeai/ecoprompt/user/controller/AuthController.java
+++ back/src/main/java/com/closeai/ecoprompt/user/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
-public class AuthController {
+public class AuthController implements AuthControllerDocs {
 
     @GetMapping("/sign-in")
     public ResponseEntity<Void> signIn(HttpServletResponse response) throws Exception {
