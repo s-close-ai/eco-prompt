dca9f1b6404ad8ac9771e76dbbb754827a911d38
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-11-08 16:09:46 +0900

    feat: 사용자 정보 설정관련 기능 구현
    - 사용자 지침 설정 기능
    - 사용자 정보 조회 기능
    - 사용자 프롬프트 공유 설정 기능

--- back/src/main/java/com/closeai/ecoprompt/userinfo/controller/UserInfoController.java
+++ back/src/main/java/com/closeai/ecoprompt/userinfo/controller/UserInfoController.java
@@ -1,30 +1,54 @@
 package com.closeai.ecoprompt.userinfo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.closeai.ecoprompt.common.ApiResponse;
+import com.closeai.ecoprompt.userinfo.model.dto.request.UpdatePersonalPromptRequest;
+import com.closeai.ecoprompt.userinfo.model.dto.response.GetUserInfoResponse;
 import com.closeai.ecoprompt.userinfo.model.dto.response.SharingInformationStatusResponse;
 import com.closeai.ecoprompt.userinfo.service.UserInfoService;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user-infos")
-public class UserInfoController {
+public class UserInfoController implements UserInfoControllerDocs {
 
-    private final UserInfoService userInfoService;
+	private final UserInfoService userInfoService;
 
-    @GetMapping("/sharing-information")
-    public ResponseEntity<ApiResponse<SharingInformationStatusResponse>> getSharingInformation() {
+	@GetMapping("/sharing-information")
+	public ResponseEntity<ApiResponse<SharingInformationStatusResponse>> getSharingInformation() {
 
-        return ApiResponse.success(userInfoService.getSharingInformationStatus());
-    }
+		return ApiResponse.success(userInfoService.getSharingInformationStatus());
+	}
 
-    @PatchMapping("/sharing-information")
-    public ResponseEntity<ApiResponse<SharingInformationStatusResponse>> toggleSharingInformation(
-    ) {
+	@PatchMapping("/sharing-information")
+	public ResponseEntity<ApiResponse<SharingInformationStatusResponse>> toggleSharingInformation() {
 
-        return ApiResponse.success(userInfoService.toggleSharingInformation());
-    }
+		return ApiResponse.success(userInfoService.toggleSharingInformation());
+	}
+
+	@PatchMapping("/personal-prompt")
+	public ResponseEntity<ApiResponse<Void>> updatePersonalPrompt(
+		@RequestBody @Valid UpdatePersonalPromptRequest request) {
+		return ApiResponse.success(userInfoService.updatePersonalPrompt(request));
+	}
+
+	@GetMapping
+	public ResponseEntity<ApiResponse<GetUserInfoResponse>> getUserInfo() {
+		return ApiResponse.success(userInfoService.getUserPromptInfo());
+	}
+
+	@PatchMapping("/sharing-prompt")
+	public ResponseEntity<ApiResponse<Void>> toggleSharingPrompt() {
+		return ApiResponse.success(userInfoService.updateSharingPrompt());
+	}
 
 }
--- back/src/main/java/com/closeai/ecoprompt/userinfo/model/entity/UserInfo.java
+++ back/src/main/java/com/closeai/ecoprompt/userinfo/model/entity/UserInfo.java
@@ -1,94 +1,114 @@
 package com.closeai.ecoprompt.userinfo.model.entity;
 
+import java.time.LocalDateTime;
+
 import com.closeai.ecoprompt.common.CustomUtil;
 import com.closeai.ecoprompt.common.entity.BaseEntity;
 import com.closeai.ecoprompt.user.model.entity.User;
-import jakarta.persistence.*;
-import lombok.*;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "user_info")
 public class UserInfo extends BaseEntity {
 
-    @Id
-    @Column(name = "user_info_id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@Column(name = "user_info_id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Column(name = "total_mileage", nullable = false)
-    private Integer totalMileage = 0;
+	@Column(name = "total_mileage", nullable = false)
+	private Integer totalMileage = 0;
 
-    @Column(name = "high_score", nullable = false)
-    private Double highScore = 0.0;
+	@Column(name = "high_score", nullable = false)
+	private Double highScore = 0.0;
 
-    @Column(name = "personal_prompt", length = 1000)
-    private String personalPrompt;
+	@Column(name = "personal_prompt", length = 1000)
+	private String personalPrompt = "";
 
-    @Column(name = "sharing_information", length = 1, nullable = false, columnDefinition = "VARCHAR(1) DEFAULT 'N'")
-    private String sharingInformation = "N";  // 기본값 N
+	@Column(name = "sharing_information", length = 1, nullable = false, columnDefinition = "VARCHAR(1) DEFAULT 'N'")
+	private String sharingInformation = "N";  // 기본값 N
 
-    @Column(name = "sharing_information_updated_at", nullable = false)
-    private String sharingInformationUpdatedAt;
+	@Column(name = "sharing_information_updated_at", nullable = false)
+	private String sharingInformationUpdatedAt;
 
-    @Column(name = "sharing_prompt", length = 1, nullable = false, columnDefinition = "VARCHAR(1) DEFAULT 'N'")
-    private String sharingPrompt = "N";  // 기본값 N
+	@Column(name = "sharing_prompt", length = 1, nullable = false, columnDefinition = "VARCHAR(1) DEFAULT 'N'")
+	private char sharingPrompt = 'N';  // 기본값 N
 
-    @Column(name="total_prompt_count", nullable = false)
-    private Long totalPromptCount = 0L;
+	@Column(name = "total_prompt_count", nullable = false)
+	private Long totalPromptCount = 0L;
 
-    @Column(name="total_score", nullable = false)
-    private Double totalScore = 0.0;
+	@Column(name = "total_score", nullable = false)
+	private Double totalScore = 0.0;
 
-    // FK: user_info.user_id -> user.user_id (1:1 가정)
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	// FK: user_info.user_id -> user.user_id (1:1 가정)
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
-    @Column(name = "prompt_count",  nullable = false)
-    private Integer promptCount = 0;
+	@Column(name = "prompt_count", nullable = false)
+	private Integer promptCount = 0;
 
-    private UserInfo(Integer totalMileage, Double highScore, int promptCount, Double totalScore, User user) {
-        this.totalMileage = totalMileage;
-        this.highScore = highScore;
-        this.promptCount = promptCount;
-        this.totalScore = totalScore;
-        this.user = user;
-        this.sharingInformationUpdatedAt = CustomUtil.dateConverter(LocalDateTime.now());
-    }
+	private UserInfo(Integer totalMileage, Double highScore, int promptCount, Double totalScore, User user) {
+		this.totalMileage = totalMileage;
+		this.highScore = highScore;
+		this.promptCount = promptCount;
+		this.totalScore = totalScore;
+		this.user = user;
+		this.sharingInformationUpdatedAt = CustomUtil.dateConverter(LocalDateTime.now());
+	}
 
-    public static UserInfo makeDefaultUserInfo(User user) {
+	public static UserInfo makeDefaultUserInfo(User user) {
 
-        return new UserInfo(
-                0,
-                0.0,
-                0,
-                0.0,
-                user
-        );
-    }
+		return new UserInfo(
+			0,
+			0.0,
+			0,
+			0.0,
+			user
+		);
+	}
 
-    public void updateSharingInformationUpdatedAt() {
-        this.sharingInformationUpdatedAt = CustomUtil.dateConverter(LocalDateTime.now());
-    }
+	public void updateSharingInformationUpdatedAt() {
+		this.sharingInformationUpdatedAt = CustomUtil.dateConverter(LocalDateTime.now());
+	}
 
-    public void updateTotalPromptCount() {
-        this.totalPromptCount++;
-    }
+	public void updateTotalPromptCount() {
+		this.totalPromptCount++;
+	}
 
-    public void updateHighScore(Double highScore) {
-        this.highScore = highScore;
-    }
+	public void updateHighScore(Double highScore) {
+		this.highScore = highScore;
+	}
 
-    public void updateTotalMileage(Integer value){
-        this.totalMileage = this.totalMileage+value;
-    }
+	public void updateTotalMileage(Integer value) {
+		this.totalMileage = this.totalMileage + value;
+	}
 
-    public void updateTotalScore(Double score){
-        this.totalScore += score;
-    }
+	public void updateTotalScore(Double score) {
+		this.totalScore += score;
+	}
+
+	public void updatePersonalPrompt(String personalPrompt) {
+		this.personalPrompt = personalPrompt;
+	}
 
 }
