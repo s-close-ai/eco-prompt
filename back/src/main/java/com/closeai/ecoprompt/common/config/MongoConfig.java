3f9d7d97fdc9556dcd788ad5e89ef89b285a46c6
이권민 <rnjsals0905@gmail.com>
2025-11-10 11:06:59 +0900

    feat: 모델 학습 스케줄링 작업
    - mongoDB createdAt 형식 변경
    - 모델 학습 스케줄러 매일 04:00에 시작

--- back/src/main/java/com/closeai/ecoprompt/common/config/MongoConfig.java
+++ back/src/main/java/com/closeai/ecoprompt/common/config/MongoConfig.java
@@ -1,6 +1,7 @@
 package com.closeai.ecoprompt.common.config;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -19,17 +20,26 @@ public class MongoConfig {
 
 	@Bean
 	public MongoCustomConversions mongoCustomConversions() {
-		return new MongoCustomConversions(List.of(new LocalDateTimeToStringConverter()));
+		return new MongoCustomConversions(List.of(
+				new LocalDateTimeToStringConverter(),
+				new StringToLocalDateTimeConverter()
+		));
 	}
 
 	private static class LocalDateTimeToStringConverter implements Converter<LocalDateTime, String> {
-
 		@Override
 		public String convert(LocalDateTime localDateTime) {
-			if(localDateTime == null) {
-				return null;
-			}
+			if (localDateTime == null) return null;
 			return CustomUtil.dateConverter(localDateTime);
+		}
+	}
+
+	private static class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
+		@Override
+		public LocalDateTime convert(String source) {
+			if (source == null) return null;
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd.HH.mm.ss");
+			return LocalDateTime.parse(source, formatter);
 		}
 	}
 }
