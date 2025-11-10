d4a4932685ae7b98e1d9f07819f215be8cab8b14
surin <tjdtnfls1128@gmail.com>
2025-11-10 15:38:48 +0900

    feat: vllm을 통한 모델 서빙 완료
    - 모델 변경: 파인튜닝한 모델의 성능이 좋지 않다고 판단하여 다른 모델(Qwen-Coder)로 변경함.
    - 배포 전 확인했을 때는, 하나의 GPU만 사용하고 있다는 것 확인함.
    - 해야할 일: 모델 재학습 로직에서 모델에 알맞은 chat template 적용하는 부분 수정 필요함.

--- llm/app/models/llm_loader.py
+++ llm/app/models/llm_loader.py
@@ -1,45 +1,17 @@
 from typing import Optional
 
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.v1.engine.async_llm import AsyncLLM
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from app.core.config import base_settings
 
 # 전역 변수 정의
-MODEL_NAME = base_settings.base_model + "/v_latest"
+MODEL_NAME = base_settings.base_model
 
-llm: Optional[AutoModelForCausalLM] = None
+
 llm_tokenizer: Optional[AutoTokenizer] = None
-
-async def load_llm():
-    """vLLM 엔진을 초기화하고 전역 변수에 할당"""
-    global llm
-
-    # 이미 있다면
-    if llm is not None:
-        print("LLM already loaded.")
-        return
-
-    # 없다면
-    print("⏳ Starting LLM Load from loader...")
-    try:
-
-        llm = AutoModelForCausalLM.from_pretrained(
-            pretrained_model_name_or_path=MODEL_NAME,
-            device_map="auto"
-        )
-        print("✅ LLM loaded successfully.")
-
-    except Exception as e:
-        print(f"❌ Failed to load LLM: {e}")       
-
-
-def get_llm() -> AutoModelForCausalLM:
-    """초기화된 LLM 객체를 반환"""
-    global llm
-
-    if llm is None:
-        raise RuntimeError("LLM Engine is not initialized.")
-    return llm
+llm_engine: Optional[AsyncLLM] = None
 
 
 def load_tokenizer():
@@ -70,3 +42,40 @@ def get_tokenizer() -> AutoTokenizer:
     if llm_tokenizer is None:
         raise RuntimeError("Tokenizer is not initialized.")
     return llm_tokenizer
+
+async def load_llm_engine():
+    """vllm으로 llm 엔진 초기화 하기"""
+    global llm_engine
+
+    # 이미 있다면
+    if llm_engine is not None:
+        print("LLM Engine already loaded.")
+        return
+
+    # 없다면
+    print("⏳ Starting LLM Engine Load from loader...")
+    
+    try:
+        engine_args = AsyncEngineArgs(
+            model=MODEL_NAME,
+            enforce_eager=True,
+            gpu_memory_utilization=0.8,
+            quantization="bitsandbytes",
+            max_model_len=8192
+        )
+
+        llm_engine = AsyncLLM.from_engine_args(engine_args)
+
+        print("✅ LLM Engine loaded successfully.")
+
+    except Exception as e:
+        print(f"❌ Failed to load LLM Engine: {e}")
+
+    
+def get_llm_engine() -> AsyncLLM:
+    """초기화된 LLM 엔진 객체를 반환"""
+    global llm_engine
+
+    if llm_engine is None:
+        raise RuntimeError("LLM Engine is not initialized.")
+    return llm_engine
