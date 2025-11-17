fde67117d3727bc12de3ba3ee5bb82e96734a081
이권민 <rnjsals0905@gmail.com>
2025-11-17 16:48:08 +0900

    feat: log lens 관련 시큐리티 컨피그 수정, 로그인 로직에서 기수 다시 한 번 확인

--- back/src/main/java/com/closeai/ecoprompt/security/oauth/SsafyOAuth2UserService.java
+++ back/src/main/java/com/closeai/ecoprompt/security/oauth/SsafyOAuth2UserService.java
@@ -108,6 +108,10 @@ public class SsafyOAuth2UserService implements OAuth2UserService<OAuth2UserReque
                         }
                 );
 
+        if (user.getEmployeeNumber().equals("UNKNOWN")) {
+            user.setEmployeeNumber(edu != null ? edu : "UNKNOWN");
+        }
+
         // 4) Security Principal (FE로 내려갈 attributes에 두 응답을 합쳐 넣으면 디버그/표시에 좋음)
         Map<String, Object> merged = new LinkedHashMap<>();
         merged.putAll(loginAttrs);
