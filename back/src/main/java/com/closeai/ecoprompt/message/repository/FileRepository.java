a5365ad2b5051eff36fb63a2942075cc135d5acc
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-11-18 22:06:35 +0900

    feat: 메시지 uuid 기준으로 파일 조회 함수 구현

--- back/src/main/java/com/closeai/ecoprompt/message/repository/FileRepository.java
+++ back/src/main/java/com/closeai/ecoprompt/message/repository/FileRepository.java
@@ -1,8 +1,11 @@
 package com.closeai.ecoprompt.message.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.closeai.ecoprompt.message.model.entity.File;
 
 public interface FileRepository extends JpaRepository<File, Long> {
+	List<File> findByMessageUUIDInAndIsDeletedFalse(List<String> messageUUIDs);
 }
